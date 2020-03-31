a887508f4679a03d7189b78b3c63211a2a4e6350
Sean Downs <spdowns@protonmail.com>
2020-03-31 09:54:49 -0700

    adds travis' help to the output div

--- js/scripts.js
+++ js/scripts.js
@@ -8,10 +8,26 @@
 //   alert ('I love ' + languages[index] + '!');
 // }
 
-var total = 0;
-var summands = [1, 2, 3, 4, 5];
-for (var index = 0; index < summands.length; index += 1) {
-  total += summands[index];
-}
+// var total = 0;
+// var summands = [1, 7, 12, 22, 15];
+// for (var index = 0; index < summands.length; index += 1) {
+//   total += summands[index];
+//   console.log(total)
+// }
 
-alert(total); 
+// alert(total); 
+$(document).ready(function() {
+  $("form").submit(function(event){
+    event.preventDefault();
+
+    var input1 = parseInt($("#input-1").val());
+    var input2 = parseInt($("#input-2").val());
+    var numbers = [];
+
+    for (index = 0; index < input1; index += input2) {
+      numbers.push(index);
+      console.log(index);
+    }
+    $(".output").append(numbers);
+  });
+});
