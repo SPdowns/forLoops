85681ae34e11a30cccc002a4cf65864c17cae212
Sean Downs <spdowns@protonmail.com>
2020-03-31 09:01:33 -0700

    we test the length property

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
-for (var index = 0; index <= 3; index += 1) {
-  alert(index);
+// for (var index = 0; index <= 3; index += 1) {
+//   alert(index);
+// }
+
+var languages = ['HTML', 'CSS', 'Javascript'];
+for (var index = 0; index < languages.length; index += 1) {
+  console.log(index)
+  alert ('I love ' + languages[index] + '!');
 }
