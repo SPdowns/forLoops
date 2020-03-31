a9b1b259e2e7eb0d4fd66ebde4827a81f4a02b89
Sean Downs <spdowns@protonmail.com>
2020-03-31 09:09:11 -0700

    adds new example

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,16 @@
 //   alert(index);
 // }
 
-var languages = ['HTML', 'CSS', 'Javascript'];
-for (var index = 0; index < languages.length; index += 1) {
-  console.log(index)
-  alert ('I love ' + languages[index] + '!');
+// var languages = ['HTML', 'CSS', 'Javascript'];
+// for (var index = 0; index < languages.length; index += 1) {
+//   console.log(index)
+//   alert ('I love ' + languages[index] + '!');
+// }
+
+var total = 0;
+var summands = [1, 2, 3, 4, 5];
+for (var index = 0; index < summands.length; index += 1) {
+  total += summands[index];
 }
+
+alert(total); 
